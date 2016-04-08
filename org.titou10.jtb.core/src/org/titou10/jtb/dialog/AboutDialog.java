8fed1de3a61749187f27a8749dd53aee24bec5ab
Denis Forveille <titou10.titou10@gmail.com>
2016-04-08 14:06:34 +0000

    Reworked the "about" dialog

--- org.titou10.jtb.core/src/org/titou10/jtb/dialog/AboutDialog.java
+++ org.titou10.jtb.core/src/org/titou10/jtb/dialog/AboutDialog.java
@@ -36,17 +36,19 @@ import org.titou10.jtb.util.Utils;
  */
 public class AboutDialog extends Dialog {
 
-   private static final String TITLE        = "Universal JMS Browser";
-   private static final String AUTHOR       = "Author: Denis Forveille";
-   private static final String CONTRIBUTOR  = "Contributors: Yannick Beaudoin";
-   private static final String VERSION      = "Version %d.%d.%d";
-   private static final String EMAIL        = "titou10.titou10@gmail.com";
-   private static final String EMAIL_MAILTO = "mailto:" + EMAIL;
-   private static final String EMAIL_LINK   = "<a href=\"" + EMAIL_MAILTO + "\">" + EMAIL + "</a>";
-   private static final String WEB          = "http://sourceforge.net/projects/jmstoolbox";
-   private static final String WEB_LINK     = "<a href=\"" + WEB + "\">" + WEB + "</a>";
+   private static final String TITLE         = "Universal JMS Browser";
+   private static final String AUTHOR        = "Author: Denis Forveille";
+   private static final String CONTRIBUTOR   = "Contributors: Yannick Beaudoin";
+   private static final String VERSION       = "Version %d.%d.%d";
+   private static final String EMAIL         = "titou10.titou10@gmail.com";
+   private static final String EMAIL_MAILTO  = "mailto:" + EMAIL;
+   private static final String EMAIL_LINK    = "<a href=\"" + EMAIL_MAILTO + "\">" + EMAIL + "</a>";
+   private static final String WEB           = "https://sourceforge.net/p/jmstoolbox";
+   private static final String WEB_LINK      = "<a href=\"" + WEB + "\">" + WEB + "</a>";
+   private static final String WEB_WIKI      = WEB + "/wiki/Home/";
+   private static final String WEB_WIKI_LINK = "<a href=\"" + WEB_WIKI + "\">" + WEB_WIKI + "</a>";
 
-   private static final String LOGO         = "icons/branding/logo-jmstoolbox.jpg";
+   private static final String LOGO          = "icons/branding/logo-jmstoolbox.jpg";
 
    public AboutDialog(Shell parentShell) {
       super(parentShell);
@@ -88,19 +90,18 @@ public class AboutDialog extends Dialog {
       lblImage.setText("Image");
       lblImage.setImage(Utils.getImage(this.getClass(), LOGO));
 
-      Label lblAuthor = new Label(container, SWT.NONE);
-      lblAuthor.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+      Composite authorComposite = new Composite(container, SWT.NONE);
+      authorComposite.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+      authorComposite.setLayout(new GridLayout(2, false));
+
+      Label lblAuthor = new Label(authorComposite, SWT.NONE);
+      lblAuthor.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
       lblAuthor.setFont(SWTResourceManager.getFont("Tahoma", 10, SWT.NORMAL));
       lblAuthor.setText(AUTHOR);
 
-      Label lblHelper = new Label(container, SWT.NONE);
-      lblHelper.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
-      lblHelper.setFont(SWTResourceManager.getFont("Tahoma", 9, SWT.NORMAL));
-      lblHelper.setText(CONTRIBUTOR);
-
-      Link emailLink = new Link(container, SWT.NONE);
+      Link emailLink = new Link(authorComposite, SWT.NONE);
       emailLink.setFont(SWTResourceManager.getFont("Tahoma", 9, SWT.NORMAL));
-      emailLink.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+      emailLink.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
       emailLink.setText(EMAIL_LINK);
       emailLink.addSelectionListener(new SelectionAdapter() {
          @Override
@@ -109,7 +110,19 @@ public class AboutDialog extends Dialog {
          }
       });
 
-      Link webLink = new Link(container, SWT.NONE);
+      Label lblHelper = new Label(container, SWT.NONE);
+      lblHelper.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+      lblHelper.setFont(SWTResourceManager.getFont("Tahoma", 9, SWT.NORMAL));
+      lblHelper.setText(CONTRIBUTOR);
+
+      Composite webContainer = new Composite(container, SWT.NONE);
+      webContainer.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+      webContainer.setLayout(new GridLayout(2, false));
+
+      Label lblWeb = new Label(webContainer, SWT.NONE);
+      lblWeb.setText("Web Site:");
+
+      Link webLink = new Link(webContainer, SWT.NONE);
       webLink.setFont(SWTResourceManager.getFont("Tahoma", 9, SWT.NORMAL));
       webLink.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
       webLink.setText(WEB_LINK);
@@ -120,11 +133,31 @@ public class AboutDialog extends Dialog {
          }
       });
 
+      Composite wikiComposite = new Composite(container, SWT.NONE);
+      wikiComposite.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+      wikiComposite.setLayout(new GridLayout(2, false));
+
+      Label lblWikiLink = new Label(wikiComposite, SWT.NONE);
+      lblWikiLink.setText("Online Manual:");
+
+      Link webWikiLink = new Link(wikiComposite, SWT.NONE);
+      webWikiLink.setFont(SWTResourceManager.getFont("Tahoma", 9, SWT.NORMAL));
+      webWikiLink.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+      webWikiLink.setText(WEB_WIKI_LINK);
+      webWikiLink.addSelectionListener(new SelectionAdapter() {
+         @Override
+         public void widgetSelected(SelectionEvent e) {
+            Program.launch(WEB_WIKI);
+         }
+      });
+
       Label lblVersion = new Label(container, SWT.NONE);
       lblVersion.setFont(SWTResourceManager.getFont("Tahoma", 10, SWT.NORMAL));
       lblVersion.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
       lblVersion.setAlignment(SWT.CENTER);
       lblVersion.setText(version);
+
+      webWikiLink.setFocus();
 
       return container;
    }
