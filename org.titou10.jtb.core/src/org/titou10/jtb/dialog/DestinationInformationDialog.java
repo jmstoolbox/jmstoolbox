5384ed2e96985a5211cd2cc29e9a2da9248399ed
Denis Forveille <titou10.titou10@gmail.com>
2017-01-28 14:20:47 +0000

    Exclude some queues from being browsed

--- org.titou10.jtb.core/src/org/titou10/jtb/dialog/DestinationInformationDialog.java
+++ org.titou10.jtb.core/src/org/titou10/jtb/dialog/DestinationInformationDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Denis Forveille titou10.titou10@gmail.com
+ * Copyright (C) 2015-2017 Denis Forveille titou10.titou10@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,6 +30,7 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
+import org.titou10.jtb.jms.model.JTBDestination;
 
 /**
  * Display Destination information
@@ -39,14 +40,16 @@ import org.eclipse.wb.swt.SWTResourceManager;
  */
 public class DestinationInformationDialog extends Dialog {
 
-   private String              destinationName;
+   private JTBDestination      jtbDestination;
    private Map<String, Object> destinationInformation;
 
-   public DestinationInformationDialog(Shell parentShell, String destinationName, Map<String, Object> destinationInformation) {
+   public DestinationInformationDialog(Shell parentShell,
+                                       JTBDestination jtbDestination,
+                                       Map<String, Object> destinationInformation) {
       super(parentShell);
       setShellStyle(SWT.TITLE | SWT.PRIMARY_MODAL);
 
-      this.destinationName = destinationName;
+      this.jtbDestination = jtbDestination;
       this.destinationInformation = destinationInformation;
    }
 
@@ -61,7 +64,20 @@ public class DestinationInformationDialog extends Dialog {
 
       Label lblQueueName = new Label(container, SWT.NONE);
       lblQueueName.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
-      lblQueueName.setText(destinationName);
+      lblQueueName.setText(jtbDestination.getName());
+
+      if (jtbDestination.isJTBQueue()) {
+         Label lbl2 = new Label(container, SWT.NONE);
+         lbl2.setText("Is browsable?");
+
+         Label lblIsBrowsable = new Label(container, SWT.NONE);
+         lblIsBrowsable.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+         lblIsBrowsable.setText(String.valueOf(jtbDestination.getAsJTBQueue().isBrowsable()));
+      }
+
+      // Empty line
+      new Label(container, SWT.NONE);
+      new Label(container, SWT.NONE);
 
       Label lblName;
       Label lblValue;
