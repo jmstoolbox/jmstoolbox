122f4f07a0752d0e99a3477a88bf8cd1d3af3980
Denis Forveille <titou10.titou10@gmail.com>
2017-03-28 22:42:17 +0000

    Fix artemis 1.x package name

--- org.titou10.jtb.qm.artemis/src/org/titou10/jtb/qm/artemis/ActiveMQArtemisQManager.java
+++ org.titou10.jtb.qm.artemis/src/org/titou10/jtb/qm/artemis/ActiveMQArtemisQManager.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.titou10.jtb.qm.activemq;
+package org.titou10.jtb.qm.artemis;
 
 import java.util.ArrayList;
 import java.util.HashMap;
