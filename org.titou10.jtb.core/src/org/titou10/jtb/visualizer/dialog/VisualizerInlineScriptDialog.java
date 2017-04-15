0d0edfb5ffcd2cfaa08fef07d03379bca8f1a0db
Denis Forveille <titou10.titou10@gmail.com>
2017-04-15 21:53:37 +0000

    Visualizers

--- org.titou10.jtb.core/src/org/titou10/jtb/visualizer/dialog/VisualizerInlineScriptDialog.java
+++ org.titou10.jtb.core/src/org/titou10/jtb/visualizer/dialog/VisualizerInlineScriptDialog.java
@@ -71,7 +71,7 @@ public class VisualizerInlineScriptDialog extends Dialog {
    @Override
    protected void configureShell(Shell newShell) {
       super.configureShell(newShell);
-      newShell.setText("Add a new 'OS Extension' visualizer");
+      newShell.setText("Add/Edit an 'Inline Script' visualizer");
    }
 
    protected Point getInitialSize() {
