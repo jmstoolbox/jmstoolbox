f4e1fcf160db22e3f107404057334bdb4de6a2a0
Denis Forveille <titou10.titou10@gmail.com>
2017-05-02 22:36:41 +0000

    Scripts code restructuring

--- org.titou10.jtb.core/src/org/titou10/jtb/script/part/ScriptsBrowserViewPart.java
+++ org.titou10.jtb.core/src/org/titou10/jtb/script/part/ScriptsBrowserViewPart.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.titou10.jtb.ui.part;
+package org.titou10.jtb.script.part;
 
 import java.util.ArrayList;
 import java.util.HashMap;
