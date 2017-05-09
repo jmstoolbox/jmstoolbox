cc7986b38e7f8d91163238772022c989a67c82fa
Denis Forveille <titou10.titou10@gmail.com>
2017-05-09 11:25:57 +0000

    Templates refactoring

--- org.titou10.jtb.core/src/org/titou10/jtb/template/part/TemplatesBrowserViewPart.java
+++ org.titou10.jtb.core/src/org/titou10/jtb/template/part/TemplatesBrowserViewPart.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.titou10.jtb.ui.part;
+package org.titou10.jtb.template.part;
 
 import java.io.File;
 import java.io.IOException;
