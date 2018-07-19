99cb066c8a95269f50e5253c57ffa96ef61605bc
titou10titou10 <titou10.titou10@gmail.com>
2018-07-19 16:56:38 -0400

    Replace deprecated class 'MDialog' in eclipse 4.8 by class 'MWindow'

--- org.titou10.jtb.core/src/org/titou10/jtb/script/handler/ScriptsManagerHandler.java
+++ org.titou10.jtb.core/src/org/titou10/jtb/script/handler/ScriptsManagerHandler.java
@@ -20,7 +20,7 @@ import javax.inject.Inject;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.basic.MDialog;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
@@ -50,9 +50,9 @@ public class ScriptsManagerHandler {
       log.debug("execute");
 
       // Reuse or create the Scripts Manager Dialog from the snippet
-      MDialog part = (MDialog) modelService.find(Constants.SM_DIALOG_SNIPPET, app);
+      MWindow part = (MWindow) modelService.find(Constants.SM_DIALOG_SNIPPET, app);
       if (part == null) {
-         part = (MDialog) modelService.cloneSnippet(app, Constants.SM_DIALOG_SNIPPET, null);
+         part = (MWindow) modelService.cloneSnippet(app, Constants.SM_DIALOG_SNIPPET, null);
 
          // Center Window (10px below on the right..
          Monitor monitor = display.getPrimaryMonitor();
