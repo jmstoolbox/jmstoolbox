d7cdf95482b6dfe65b5ec863de0cfee6d4b6252c
Denis Forveille <titou10.titou10@gmail.com>
2019-04-26 10:47:38 -0400

    Solace way to browse messages

--- org.titou10.jtb.core/src/org/titou10/jtb/jms/model/JTBConnection.java
+++ org.titou10.jtb.core/src/org/titou10/jtb/jms/model/JTBConnection.java
@@ -688,6 +688,9 @@ public class JTBConnection {
       try (QueueBrowser browser = jmsSession.createBrowser(jtbQueue.getJmsQueue(), selectorsSearchText);) {
          int n = 0;
 
+         // Message message = null;
+         // loopOnMessages: while ((message = (Message) msgs.nextElement()) != null) {
+
          Enumeration<?> msgs = browser.getEnumeration();
          loopOnMessages: while (msgs.hasMoreElements()) {
             Message message = (Message) msgs.nextElement();
