c96efd5a2db4dc823c2a41dc9494fedf6753b3da
rikschaaf <R.Schaaf@cjib.nl>
2020-11-09 18:54:39 +0100

    JTB-48: implement a first draft of the Chocolatey package
    This includes the nuspec with the information about the package contents, a copy of the licence in LICENCE.txt and info how to verify the contents of the package in VERIFICATION.txt
    
    This excludes the embedded jmstoolbox.zip. This will need to be added to the tools directory when running `choco pack`

---
+++ chocolatey/jmstoolbox/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'JMS Toolbox'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'jmstoolbox.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  file         = $fileLocation
+  destination   = $toolsDir
+
+  checksum      = 'F32FCAA6B09B9FB8846ECC46965EB1C8BD3A0D8E60A607E335C224B69FDC67E4'
+  checksumType  = 'sha256'
+}
+
+Get-ChocolateyUnzip @packageArgs
